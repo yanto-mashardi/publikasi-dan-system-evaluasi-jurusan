2fccf87ae69af9939380b5874cb5801765c60855
yanto-mashardi <yanto.mashardi@gmail.com>
2026-08-23 16:31:16 +0700

    Register accreditation schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,9 +5,10 @@ import * as adminSchema from "./schema-admin";
 import * as phase5Schema from "./schema-phase5";
 import * as phase6Schema from "./schema-phase6";
 import * as phase6PublicationSchema from "./schema-phase6-publication";
+import * as accreditationSchema from "./schema-accreditation";
 
 const databaseUrl=process.env.DATABASE_URL;
 const pool=databaseUrl?mysql.createPool(databaseUrl):null;
-const schema={...coreSchema,...adminSchema,...phase5Schema,...phase6Schema,...phase6PublicationSchema};
+const schema={...coreSchema,...adminSchema,...phase5Schema,...phase6Schema,...phase6PublicationSchema,...accreditationSchema};
 export const db=pool?drizzle(pool,{schema,mode:"default"}):null;
 export function requireDb(){if(!db)throw new Error("DATABASE_URL belum dikonfigurasi.");return db;}
