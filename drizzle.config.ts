1df59cd015609aa9324af70b39614e9804c3210d
yanto-mashardi <yanto.mashardi@gmail.com>
2026-08-23 13:57:42 +0700

    Include dynamic admin schema in migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/db/schema.ts",
+  schema: ["./src/db/schema.ts","./src/db/schema-admin.ts"],
   out: "./drizzle",
   dialect: "mysql",
   dbCredentials: {
