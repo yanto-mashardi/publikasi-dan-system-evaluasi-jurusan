8a084c737fb5ceec28a789172e6e06aa6a235a31
yanto-mashardi <yanto.mashardi@gmail.com>
2026-08-23 14:08:16 +0700

    Remove unused dynamic auth helper

--- src/lib/auth-dynamic.ts
+++
@@ -1,7 +0,0 @@
-export type UserScope={role:string;organizationId:number;studyProgramId?:number|null};
-
-export type DynamicSessionShape={
-  roles:string[];
-  permissions:string[];
-  scopes:UserScope[];
-};
